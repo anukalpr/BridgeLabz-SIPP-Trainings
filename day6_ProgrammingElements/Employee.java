232a8dead1fc4e3306f5e372e7bc6b658f3bbdfc
anukalpr <singhanukalp465@gmail.com>
2025-08-09 22:10:09 +0530

    [Anukalp] Add Employee

---
+++ day6_ProgrammingElements/Employee.java
@@ -0,0 +1,52 @@
+package day6_ProgrammingElements;
+
+public class Employee {
+    private String name;
+    private int id;
+    private double salary;
+
+    public Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void displayDetails() {
+        System.out.println("Employee Details:");
+        System.out.println("Name   : " + name);
+        System.out.println("ID     : " + id);
+        System.out.println("Salary : ₹" + salary);
+    }
+
+    public static void main(String[] args) {
+        Employee emp1 = new Employee("Alice", 101, 55000);
+        Employee emp2 = new Employee("Bob", 102, 60000);
+        emp1.displayDetails();
+        System.out.println();
+        emp2.displayDetails();
+    }
+}
