265a81e2938892af650a6fd6ba663aec03bc12ef
anukalpr <singhanukalp465@gmail.com>
2025-08-09 22:29:03 +0530

    [Anukalp] Add Vehicle

---
+++ day7_ProgrammingElements/Vehicle.java
@@ -0,0 +1,39 @@
+package day7_ProgrammingElements;
+
+public class Vehicle {
+    private String ownerName;
+    private String vehicleType;
+    private static double registrationFee = 500.0;
+
+    public Vehicle(String ownerName, String vehicleType) {
+        this.ownerName = ownerName;
+        this.vehicleType = vehicleType;
+    }
+
+    public void displayVehicleDetails() {
+        System.out.println("Owner Name: " + ownerName);
+        System.out.println("Vehicle Type: " + vehicleType);
+        System.out.println("Registration Fee: $" + registrationFee);
+    }
+
+    public static void updateRegistrationFee(double newFee) {
+        registrationFee = newFee;
+    }
+
+    public static void main(String[] args) {
+        Vehicle v1 = new Vehicle("Alice", "Car");
+        Vehicle v2 = new Vehicle("Bob", "Motorcycle");
+
+        System.out.println("Before Fee Update:");
+        v1.displayVehicleDetails();
+        System.out.println();
+        v2.displayVehicleDetails();
+
+        Vehicle.updateRegistrationFee(600.0);
+
+        System.out.println("\nAfter Fee Update:");
+        v1.displayVehicleDetails();
+        System.out.println();
+        v2.displayVehicleDetails();
+    }
+}
