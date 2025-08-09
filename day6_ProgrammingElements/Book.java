edd6a2e0be8d079921569f8bf5f1c8d720ee2212
anukalpr <singhanukalp465@gmail.com>
2025-08-09 22:11:45 +0530

    [Anukalp] Add Book

---
+++ day6_ProgrammingElements/Book.java
@@ -0,0 +1,52 @@
+package day6_ProgrammingElements;
+
+public class Book {
+    private String title;
+    private String author;
+    private double price;
+
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void displayDetails() {
+        System.out.println("Book Details:");
+        System.out.println("Title  : " + title);
+        System.out.println("Author : " + author);
+        System.out.println("Price  : ₹" + price);
+    }
+
+    public static void main(String[] args) {
+        Book book1 = new Book("Java Programming", "James Gosling", 499.99);
+        Book book2 = new Book("Effective Java", "Joshua Bloch", 899.50);
+        book1.displayDetails();
+        System.out.println();
+        book2.displayDetails();
+    }
+}
