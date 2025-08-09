3a8cc6c0aa4b99fedf4145110aa22f3d089736ff
anukalpr <singhanukalp465@gmail.com>
2025-08-09 22:25:31 +0530

    [Anukalp] Add Book

---
+++ day7_ProgrammingElements/Book.java
@@ -0,0 +1,35 @@
+package day7_ProgrammingElements;
+
+public class Book {
+    private String title;
+    private String author;
+    private double price;
+
+    public Book() {
+        this.title = "Unknown";
+        this.author = "Unknown";
+        this.price = 0.0;
+    }
+
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public void displayDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: $" + price);
+    }
+
+    public static void main(String[] args) {
+        Book defaultBook = new Book();
+        Book customBook = new Book("Java Programming", "James Gosling", 599.99);
+
+        System.out.println("Default Book:");
+        defaultBook.displayDetails();
+
+        System.out.println("\nCustom Book");
+    }
+}
